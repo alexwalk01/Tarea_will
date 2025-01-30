0deda78950c09375f20b967eed700c9d40b4f6e6
GuillenME <guillenmariana550@gmail.com>
2025-01-30 09:30:33 -0600

    Cambios de restriccion

---
+++ database/migrations/2025_01_30_151248_create_proyectos_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up() {
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Relación con el usuario
+            $table->timestamps();
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('proyectos');
+    }
+};
+
