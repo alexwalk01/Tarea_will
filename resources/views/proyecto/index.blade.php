b2fcff6201526d0acf4821205ed30d65e61ad9aa
alexwalk01 <alexitokira.prod@gmail.com>
2025-01-31 14:55:44 -0600

    cambios 2.1

--- resources/views/proyecto/index.blade.php
+++ resources/views/proyecto/index.blade.php
@@ -3,20 +3,31 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        @include('layouts._sidebar') <!-- Se incluye el menú aquí -->
+        @include('layouts._sidebar') <!-- Se incluye el menú -->
 
         <main class="col-12 col-md-9 col-lg-10 content">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="{{ route('menu.index') }}">Inicio</a></li>
-                    <li class="breadcrumb-item"><a href="{{ route('proyecto.index') }}">Proyectos</a></li>
-                    <li class="breadcrumb-item active">{{ $proyecto->nombre }}</li>
+                    <li class="breadcrumb-item active" aria-current="page">Proyectos</li>
                 </ol>
             </nav>
 
             <div class="container">
-                <h2>{{ $proyecto->nombre }}</h2>
-                <p><strong>Descripción:</strong> {{ $proyecto->descripcion }}</p>
+                <h2>Proyectos disponibles</h2>
+                @if ($proyectos->isEmpty())
+                    <p>No tienes proyectos disponibles.</p>
+                @else
+                    <ul>
+                        @foreach ($proyectos as $proyecto)
+                            <li>
+                                <a href="{{ route('proyecto.show', $proyecto->id) }}">
+                                    {{ $proyecto->nombre }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                @endif
             </div>
         </main>
     </div>
