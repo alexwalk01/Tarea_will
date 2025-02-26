b80c0c8444bca207ff6847576b626b1aa0582594
Froilan Najera Morales <froilannajeramorales@gmail.com>
2025-02-25 18:25:07 -0600

    quite el buscador en lo de recuperar contraseña

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,17 +25,14 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto">
+                    <ul class="navbar-nav me-auto"></ul>
 
-                    </ul>
-
-                    @if (!request()->is('login'))  {{-- Condición para ocultar el buscador en la ruta de login --}}
+                    @if (!request()->is('login') && !request()->is('password/reset') && !request()->is('password/email'))
                         <form action="{{ route('buscar.general') }}" method="GET" class="d-flex" style="margin-left: auto; margin-right: 200px; gap: 5px; align-items: center; font-size: 12px;">
                             <input required type="text" name="nombre" placeholder="Búsqueda general..." value="{{ request('nombre') }}" style="padding: 3px; font-size: 12px;">
                             <button type="submit" style="padding: 3px 8px; font-size: 12px; cursor: pointer;">Buscar</button>
                         </form>
                     @endif
-
 
                     <ul class="navbar-nav ms-auto">
                         @guest
