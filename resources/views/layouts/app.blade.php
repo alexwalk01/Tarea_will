837a92db180bca1fd7fad711f8dcc8f308f45d00
hrubendc2001214 <hugorubendiazcruz12014@gmail.com>
2025-02-28 17:26:38 -0600

    correccion del  menu y boton para cerrar sesion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,6 @@
 
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" defer></script>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
